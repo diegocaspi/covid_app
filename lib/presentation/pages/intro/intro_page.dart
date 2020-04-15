6fc00bd66ae7fa843f0b370c5d9e56dc349f9132
diegocaspi_ <diegocaspi2002@gmail.com>
2020-04-15 17:00:54 +0200

    Initialized intro & home pages, TODO: implement Di

---
+++ lib/presentation/pages/intro/intro_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  IntroPage({Key key}) : super(key: key);
+  
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
